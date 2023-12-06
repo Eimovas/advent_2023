1c40a028e85b3ebb7d081a77f23956ba8783da2b
eimantas <e.stankevicius1@gmail.com>
2023-12-06 22:41:06 +0000

    Adding some zig

--- day_4/part_1.zig
+++ day_4/part_1.zig
@@ -1,10 +1,14 @@
 const std = @import("std");
 
 pub fn main() !void {
-    std.debug.print("Hello world!\n", .{});
+    parse_game("what the hell");
 }
 
-pub fn parse_game(line: []u8) void {
-    _ = line;
+pub fn parse_game(line: []const u8) void {
+    var split = std.mem.split(u8, line, " ");
+    while (split.next()) |x| {
+        std.debug.print("word: {s}\n", .{x});
+    }
+
     // TODO: FINISHED HERE!
 }
